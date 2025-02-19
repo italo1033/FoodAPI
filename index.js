ef566d09f697a6f698cb5fcbba3c665ab6711ab6
Italo1033 <italogammy1033@gmail.com>
2025-02-18 22:34:28 -0300

    todas imagens

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('./src/app');
 const port = 3000;
 
-app.listen(port, () => {
+
+app.listen(port,'0.0.0.0', () => {
   console.log(`API rodando em http://localhost:${port}`);
 });
